f4b027f66990594916ab94dd02ae3b0ecc807dd0
sirinapha88 <sirinapha888@gmail.com>
2015-11-08 13:32:45 -0800

    add script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-// Your JS goes here
+
+var makeSquare = function(color) {
+    var myDiv = document.createElement("div");
+    document.body.appendChild(myDiv);
+    myDiv.className = "divBody";
+    myDiv.style.width = '11.1%';
+	myDiv.style.paddingBottom = ' 11.1%';
+	myDiv.style.float = 'left';
+	myDiv.style.backgroundColor = color;
+};
+
+var theBody = document.getElementsByTagName("body");
+randomColor();
+
+function redBoard(){
+
+	for(var i = 0; i < 63; i++){
+
+		if(i%2 === 0){
+			makeSquare('black');
+		} else {
+			
+			makeSquare('red');
+		}
+
+	}
+}
+
+
+function randomColor(){
+	for(var i = 0; i < 63; i++){
+		
+		makeSquare("#"+((1<<24)*Math.random()|0).toString(16));	
+
+	}	
+}
+
+
+
+
+
